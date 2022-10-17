0e56c88552b69df76d7735941af7cc31bf693d29
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-10-17 23:50:21 +0100

    Add files via upload

---
+++ starter_code/frontend/src/environments/environment.ts
@@ -0,0 +1,14 @@
+/* @TODO replace with your variables
+ * ensure all variables on this page match your project
+ */
+
+export const environment = {
+  production: false,
+  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  auth0: {
+    url: 'dev-3w74cpdd.us.auth0.com', // the auth0 domain prefix
+    audience: 'dev', // the audience set for the auth0 app
+    clientId: 'VMNXcztCVfsI3d7BJQpPUmAJceDIJZ5n', // the client id generated for the auth0 app
+    callbackURL: 'https://127.0.0.1:8080/login-results', // the base url of the running ionic application.
+  }
+};
